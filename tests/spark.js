727e4f15fadf15b5659b17d21d29cee9aa3c7c78
Tyler Garlick <tjgarlick@gmail.com>
2014-03-20 16:58:36 -0600

    work on document operations

--- tests/spark.js
+++ tests/spark.js
@@ -1,126 +1,63 @@
 'use strict';
-
-var _ = require('lodash');
-
-var spark = require('../');
-var Collection = require('../lib/collection');
-var databaseUrl = 'http://localhost:8000';
-var databaseName = 'testing';
+var Spark = require('../lib/spark');
+var util = require('util');
 
 describe('Spark', function () {
+  describe('constructor', function () {
+    it('should save the constructor parameters', function () {
+      // with parameters
+      var spark = new Spark({url: 'http://localhost:8000', database: 'testing'});
 
-  describe('initialize', function () {
-    it('should set the configuration parameters', function () {
-      spark.initialize(databaseUrl, databaseName);
-      spark.configuration.database.url.should.be.eql(databaseUrl, 'Database url should be ' + databaseUrl);
-      spark.configuration.database.name.should.be.eql(databaseName, 'Database name should be ' + databaseName);
+      spark.config.can('url').should.be.ok;
+      spark.config.can('database').should.be.ok;
+
+      spark.config.get('url').should.be.eql('http://localhost:8000');
+      spark.config.get('database').should.be.eql('testing');
+
+      // without parameters
+      spark = new Spark();
+
+      spark.config.can('url').should.be.ok;
+      spark.config.can('database').should.be.ok;
+
+      spark.config.get('url').should.be.eql('http://localhost:8000');
+      spark.config.get('database').should.be.eql('testing');
+
     });
-    it('should require the database url', function () {
-      spark.configuration.reset();
-      (function () {
-        spark.initialize('', databaseName);
-      }).should.throw();
+  });
+
+  describe('all', function () {
+    it('should return all the documents from a collection', function (done) {
+      var spark = new Spark({url: 'http://localhost:8000', database: 'testing'});
+      spark.all('first')
+        .then(function (docs) {
+          done();
+        }, function (err) {
+          console.log(util.inspect(err));
+          done(err);
+        })
+    });
+  });
+
+  describe('query', function () {
+    var spark;
+    before(function (done) {
+      spark = new Spark({url: 'http://localhost:8000', database: 'testing'});
+      done();
+    });
+    it('should be able to execute a valid aql query', function (done) {
+      spark.query('FOR p IN people RETURN p')
+        .then(function (people) {
+          people.should.be.ok;
+          people.length.should.be.eql(2);
+          done();
+        }, function (err) {
+          console.log(err);
+          done(err);
+        });
+
     });
   });
 
 
-
-  describe('collections', function () {
-    beforeEach(function () {
-      spark.initialize(databaseUrl, databaseName, { username: 'root' })
-      spark.should.be.ok;
-      spark.configuration.database.url.should.be.eql(databaseUrl, 'Database url should be ' + databaseUrl);
-      spark.configuration.database.name.should.be.eql(databaseName, 'Database name should be ' + databaseName);
-    });
-
-    describe('all', function () {
-      it('should return all collections that are non system specific by default', function (finished) {
-        spark.collections.all().then(function (collections) {
-            collections.should.be.ok;
-            _.contains(_.pluck(collections, 'name'), 'first').should.be.true;
-            finished();
-          },
-          function (err) {
-            console.log(err);
-            finished(err);
-          });
-      });
-      it('should return all collections including system collections when excludeSystem is set to false', function (finished) {
-        spark.collections.all({ excludeSystem: false })
-          .then(function (collections) {
-            collections.should.be.ok;
-            _.contains(_.pluck(collections, 'name'), '_users').should.be.true;
-            finished();
-          },
-          function (err) {
-            finished(err);
-          });
-      });
-    });
-
-    describe('get', function () {
-      it('should get a collection by name when the collection exists', function (done) {
-        spark.collections.get('first')
-          .then(function (collection) {
-            collection.should.be.ok;
-            collection.name.should.be.eql('first');
-            done();
-          },
-          function (err) { done(err);});
-      });
-    });
-
-    describe('exists', function () {
-      it('should be true if the collection exists', function (done) {
-        spark.collections.exists('first')
-          .then(function (exists) {
-            exists.should.be.true;
-            done();
-          },
-          function (err) { done(err);});
-      });
-      it('should be false if the collection does not exist', function (done) {
-        spark.collections.exists('bogus')
-          .then(function (exists) {
-            exists.should.be.false;
-            done();
-          },
-          function (err) { done(err);});
-      });
-    });
-
-    describe('create', function () {
-
-      beforeEach(function (done) {
-        spark.collections.destroy('people')
-          .then(function (result) {
-            done();
-          }, function (err) {
-            done(err)
-          });
-      });
-
-      it('should be able to create a new collection', function (done) {
-        spark.collections.create('people')
-          .then(function (collection) {
-            spark.collections.exists('people')
-              .then(function (exists) {
-                if (!exists) done('The collection people was not created successfully')
-                else done();
-              }, function (err) {
-                done(err);
-              });
-          }, function (err) {
-            done(err);
-          });
-      });
-    });
-  });
-
-  describe('collection', function () {
-    it('should have a documents array', function () {
-      var collection = new Collection({name: 'people'});
-      console.log(collection.documents);
-    });
-  });
 });
--- tests/utilities.js
+++ tests/utilities.js
@@ -1,16 +1,60 @@
 'use strict';
+var utilities = require('../lib/utilities');
 
-var helpers = require('../lib/helpers');
-var databaseUrl = 'http://proton.arangohosting.com';
+describe('utilities', function () {
 
-describe('Utilities', function () {
-  describe('database', function () {
-    it('should be able to ping a valid server', function (done) {
-      helpers.database.ping(databaseUrl).then(function () { done();}, function (err) { done(err)});
-      done();
+  describe('url', function () {
+
+    describe('build', function () {
+      it('should build out a url with the database useful for the api url', function(){
+        utilities.url.build('http://localhost', 'the-database').should.be.eql('http://localhost/_db/the-database');
+      });
     });
-  });
-  describe('urls', function () {
+
+    describe('combine', function () {
+      it('should combine a url and a path with an ending slash on the url and a beginning slash on the path', function () {
+        var url = 'http://localhost/';
+        var path = '/the/path/of/awesomeness';
+        var result = utilities.url.combine(url, path);
+        result.should.be.eql('http://localhost/the/path/of/awesomeness');
+      });
+      it('should combine a url and a path with no ending slash and a url with no ending slash', function () {
+        var url = 'http://localhost';
+        var path = 'the/path/of/awesomeness';
+        var result = utilities.url.combine(url, path);
+        result.should.be.eql('http://localhost/the/path/of/awesomeness');
+      });
+    });
+
+    describe('validate', function () {
+      it('should whether a url starts with the http protocol', function () {
+        utilities.url.validate('http://localhost').should.be.ok;
+      });
+      it('should throw a SparkError if the url is invalid', function () {
+        (function () {
+          utilities.url.validate('bad://url')
+        }).should.throw();
+      });
+    });
+
+    describe('interpolate', function () {
+      it('should replace parameters in url', function () {
+        utilities.url.interpolate('http://localhost/{db}', { db: 'awesome'}).should.be.eql('http://localhost/awesome');
+      });
+      it('should work even if the parameters are not passed in with the params', function () {
+        utilities.url.interpolate('http://localhost/{noParam}', {}).should.be.eql('http://localhost/{noParam}');
+      });
+    });
+
+    describe('removeTrailingSlash', function () {
+      it('should remove the trailing slash from a url', function () {
+        utilities.url.removeTrailingSlash('http://localhost/').should.be.eql('http://localhost');
+      });
+      it('should return the url if there is no trailing slash', function () {
+        utilities.url.removeTrailingSlash('http://localhost').should.be.eql('http://localhost');
+      });
+    });
+
 
   });
 });
