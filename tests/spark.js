4f1b83d3c27e23c479ec33cd913d93516e129100
Tyler Garlick <tjgarlick@gmail.com>
2014-04-01 05:43:12 -0600

    work on findById and find

--- tests/spark.js
+++ tests/spark.js
@@ -59,5 +59,72 @@ describe('Spark', function () {
     });
   });
 
+  describe('find', function () {
+    var spark;
+    before(function (done) {
+      spark = new Spark({url: 'http://localhost:8000', database: 'testing'});
+      done();
+    });
+
+    it('should be able to find billy by the name predicate', function (done) {
+      spark.find('people', {name: 'billy'})
+        .then(function (people) {
+          people.should.be.ok;
+          people.length.should.be.eql(1);
+          people[0].name.should.be.eql('billy');
+          done();
+        }, function (err) {
+          console.log(err);
+          done(err);
+        });
+
+
+    });
+
+    it('should return an empty array if none are found', function (done) {
+      spark.find('people', {name: 'ohBilly'})
+        .then(function (people) {
+          people.should.be.ok;
+          people.length.should.be.eql(0);
+          done();
+        }, function (err) {
+          console.log(err);
+          done(err)
+        });
+    });
+  });
+
+  describe('findById', function () {
+    var spark;
+    before(function (done) {
+      spark = new Spark();
+      done();
+    });
+
+
+    it('should be able to find a document by its id', function (done) {
+      var documentId = '93077927';
+      spark.findById('people', documentId)
+        .then(function (person) {
+          person.should.be.ok;
+          person.name.should.be.eql('bob');
+          done();
+        }, function (err) { done(err);})
+    });
+
+    it('should throw an error if the document is not found', function (done) {
+      var documentId = '11111111';
+      (function(){
+        spark.findById('people', documentId);
+      }).should.not.throw();
+//      (function () {
+//        spark.findById('people', documentId);
+//
+//      }).should.throw();
+      done();
+
+    });
+
+  })
 
 });
